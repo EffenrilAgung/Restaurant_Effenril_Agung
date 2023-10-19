2b2e9646dccba8144c37e4c89fef26399f9220fb
EffenrilAgung <effenrilagung300@gmail.com>
2023-10-20 05:10:28 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./pages/**/*.{html,js}', './assets/**/*.{js}'],
+	theme: {
+		extend: {
+			screens: {
+				'sm-380': '380px',
+				'sm-414': '414px',
+				sm: '640px',
+				// => @media (min-width: 640px) { ... }
+
+				md: '768px',
+				'md-938': '938px',
+				// => @media (min-width: 768px) { ... }
+
+				lg: '1024px',
+				// => @media (min-width: 1024px) { ... }
+			},
+			colors: {
+				astral: '#3B677F',
+				'astral-100': '#3b677f',
+				outerSpace: ' rgb(38, 60, 73)',
+			},
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif'],
+				comorant: ['Cormorant Garamond', 'serif'],
+				'notto-serif': ['Noto Serif', 'serif'],
+			},
+			backgroundImage: {
+				'background-login':
+					"url('https://images.squarespace-cdn.com/content/v1/62263fbc76242e02ac3caf10/03cf7ab4-86c9-4dd8-9fa3-5f4ebaa12481/Rosewater-Front.jpg')",
+				'background-sigup':
+					"url('https://images.squarespace-cdn.com/content/v1/62263fbc76242e02ac3caf10/f8917fbe-5679-44c8-b4fb-56a2d49f810e/Elle-Max-9930.jpg')",
+				'background-reservation': "url('./Pictures/rainbow.jpg')",
+				'background-bartender': "url('./Pictures/bartender.jpg')",
+				//  'background-reservation':
+				//  	"url('https://images.squarespace-cdn.com/content/v1/62263fbc76242e02ac3caf10/1655219631527-RNYFEBMBA825QC17022F/Climate+Cafe.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
